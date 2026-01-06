982673d7147ec13e0084de2a98fc24380e45529f
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2026-01-06 15:21:24 +0100

    forgot on both parts of the test

--- test/mar/test-mle.jl
+++ test/mar/test-mle.jl
@@ -135,7 +135,7 @@ end
 
     @test norm(sigma1_updated - model.Sigma1) < 0.1
     @test norm(dgp.Sigma1 - sigma1_updated) < 0.1
-    @test size(sigma1_updated) == model.Sigma1
+    @test size(sigma1_updated) == size(model.Sigma1)
 
     sigma2_updated = update_Sigma2(data, model.A, model.B, sigma1_updated)
 

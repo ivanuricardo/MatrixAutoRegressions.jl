3c8e609e7fbdda68f4ea86dafc68d08f9163d657
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2026-05-04 09:06:29 +0200

    improving speed of mle and als objective

--- src/mar/mar.jl
+++ src/mar/mar.jl
@@ -113,10 +113,22 @@ function fit!(model::MAR)
 end
 
 function ls_objective(resp::AbstractArray{T}, pred::AbstractArray{T}, A::AbstractMatrix{T}, B::AbstractMatrix{T}) where T
+    n1 = size(resp, 1)
+    n2 = size(resp, 2)
+    n2p = size(pred, 2)
     obs = size(resp, 3)
-    ssr = 0
+    ssr = zero(T)
+
+    tmp_AP = zeros(T, n1, n2p)    # (n1 × n2*p), not (n1 × n2)
+    residual = zeros(T, n1, n2)
+
     for i in 1:obs
-        residual = resp[:, :, i] - A * pred[:, :, i] * B'
+        Ri = resp[:, :, i]
+        Pi = pred[:, :, i]
+
+        mul!(tmp_AP, A, Pi)            # (n1 × n2*p)
+        mul!(residual, tmp_AP, B')     # (n1 × n2*p) * (n2*p × n2) = (n1 × n2)
+        residual .= Ri .- residual
         ssr += sum(abs2, residual)
     end
     return ssr
@@ -124,17 +136,22 @@ end
 
 function ls_objective(data::AbstractArray{T}, A::Vector{<:AbstractMatrix}, B::Vector{<:AbstractMatrix}; p=1) where T
     n1, n2, obs = size(data)
+    ssr = zero(T)
 
-    ssr = 0
+    pred = zeros(T, n1, n2)
+    tmp_AX = zeros(T, n1, n2)
+    residual = zeros(T, n1, n2)
+
     for i in (p+1):obs
-        pred = zeros(n1, n2)
+        fill!(pred, zero(T))
         for j in 1:p
-            pred .+= A[j] * data[:, :, i-j] * B[j]'
+            X_t = data[:, :, i-j]
+            mul!(tmp_AX, A[j], X_t)
+            mul!(pred, tmp_AX, B[j]', one(T), one(T))
         end
-        residual = data[:, :, i] - pred
+        residual .= data[:, :, i] .- pred
         ssr += sum(abs2, residual)
     end
-    
     return ssr
 end
 
@@ -150,24 +167,45 @@ function mle_objective(
     Sigma1::AbstractMatrix,
     Sigma2::AbstractMatrix,
     ) where T
-
     n1, n2, obs = size(data)
     p = length(A)
+    ssr = zero(T)
 
-    ssr = 0
+    S1_chol = cholesky(Symmetric(Sigma1))
+    S2_chol = cholesky(Symmetric(Sigma2))
+
+    pred = zeros(T, n1, n2)
+    residual = zeros(T, n1, n2)
+    tmp_AX = zeros(T, n1, n2)
+    tmp_S1r = zeros(T, n1, n2)    # Sigma1 \ residual
+    tmp_S1rS2 = zeros(T, n1, n2)  # (Sigma1 \ residual) / Sigma2
 
     for i in (p+1):obs
-        pred = zeros(n1, n2)
+        fill!(pred, zero(T))
         for j in 1:p
-            pred .+= A[j] * data[:, :, i-j] * B[j]'
+            X_t = data[:, :, i-j]
+            mul!(tmp_AX, A[j], X_t)
+            mul!(pred, tmp_AX, B[j]', one(T), one(T))
         end
 
-        residual = data[:, :, i] - pred
-        ssr += tr((Sigma1 \ residual) / Sigma2 * residual')
+        residual .= data[:, :, i] .- pred
+
+        tmp_S1r .= residual
+        ldiv!(S1_chol, tmp_S1r)
+
+        tmp_S1rS2 .= tmp_S1r
+        rdiv!(tmp_S1rS2, S2_chol)
+
+        for col in 1:n2
+            for row in 1:n1
+                ssr += tmp_S1rS2[row, col] * residual[row, col]
+            end
+        end
     end
+
     eff_obs = obs - p
-    first_cov = -n2 * eff_obs * logdet(Sigma1)
-    second_cov = -n1 * eff_obs * logdet(Sigma2)
+    first_cov = -n2 * eff_obs * logdet(S1_chol)
+    second_cov = -n1 * eff_obs * logdet(S2_chol)
 
     return first_cov + second_cov - ssr
 end
